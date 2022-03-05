f7ebcd26af58a799cab3a8fc08e94cdf28fe7fab
Bijoy Anil <86096110+bijoy7034@users.noreply.github.com>
2022-03-05 13:52:29 +0530

    1
    1

---
+++ app.js
@@ -0,0 +1,8 @@
+nav_services = document.querySelector("#services")
+nav_about = document.querySelector("#about")
+nav_services.addEventListener('click' , function(){
+    window.scrollBy(0,640);
+});
+nav_about.addEventListener('click' , function(){
+    window.scrollBy(0,2040);
+});
